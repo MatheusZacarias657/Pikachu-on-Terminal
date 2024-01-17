699c2a064f185d06a9defd7ce133bb3d3f75a9af
MatheusZacarias657 <74462682+MatheusZacarias657@users.noreply.github.com>
2024-01-16 22:44:09 -0300

    Create pikachu.c

---
+++ pikachu.c
@@ -0,0 +1,61 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main(void){
+
+    pikachu();
+    getch();
+}
+
+void pikachu (void) {
+
+    printf("                                                                                                                       \n");
+    printf("EEGGii                                                                                                                 \n");
+    printf("tt####KKtt                                                                                                             \n");
+    printf("  KK####iiLL;;                                                                                                         \n");
+    printf("  ii####;;,,JJJJ                                                                                                       \n");
+    printf("    LL##tt,,,,;;LL..                                            ..ttGGKKWWWWWWWWWW                                     \n");
+    printf("      KKGG,,,,,,,,LL..                                    ..ttjjjj;;,,EE########ii                                     \n");
+    printf("      ..DD,,,,,,,,,,JJ..                              ..JJJJ,,,,,,,,,,;;######ii                                       \n");
+    printf("        ..LL,,,,,,,,,,JJ..    iittjjjjjjjjtt;;        ttjj,,,,,,,,,,,,GG##WW;;                                         \n");
+    printf("          ..LL;;,,,,,,,,ffjjfftt,,,,,,,,,,,,ttjjjjttjj,,,,,,,,,,,,,,;;##GG..            ..tt                           \n");
+    printf("              fftt,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,tt,,,,,,,,,,,,,,iiLLii              ttjjiiii                         \n");
+    printf("                iiLLtt;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,JJJJii              iiLL;;,,,,jj                         \n");
+    printf("                    LL,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,jjGGLLtt..              ..fftt,,,,,,,,;;tt                       \n");
+    printf("                  iiii,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;;LL                  ttff,,,,,,,,,,,,,,jj                       \n");
+    printf("                  jj,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,jj..            ..LLii,,,,,,,,,,,,,,,,iiii                     \n");
+    printf("                  jj,,jjtt,,,,,,,,,,,,,,,,,,tt;;,,,,,,,,;;tt          ttjj,,,,,,,,,,,,,,,,,,,,,,jj                     \n");
+    printf("                  jjff##WW,,,,,,,,,,,,,,,,DD####ii,,,,,,,,jj      ..LLii,,,,,,,,,,,,,,,,,,,,,,,,iiii                   \n");
+    printf("                  jjGG##WW,,,,,,,,,,,,,,,,WW####jj,,,,,,,,jj    ;;LL,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,jj                   \n");
+    printf("                iitt;;DDjj,,,,,,,,,,,,,,,,ff##WW;;,,,,,,,,jj      LL,,,,,,,,,,,,,,,,,,;;;;;;;;;;jjGG                   \n");
+    printf("                LLLL,,,,,,,,,,tt,,,,,,,,,,,,,,,,iitt,,,,,,jj      ;;tt,,,,,,;;;;iittjjjjjjffGGLLii                     \n");
+    printf("                EEEE,,,,,,,,,,,,,,,,,,,,,,,,,,ffEEEEEE,,,,jj        jj,,,,jjjjjjjjjjffGGLLii                           \n");
+    printf("                DDDD,,,,,,ffLLLLffjjii,,,,,,,,EEGGGGDDjj,,tt::        LL,,iijjffGGLLii                                 \n");
+    printf("                jjGG,,,,,,,,,,,,,,,,,,,,,,,,;;EEGGGGDDff,,iiii        ;;tt;;jjGG                                       \n");
+    printf("                  LL,,,,,,,,,,,,,,,,,,,,,,,,,,LLEEEEEE,,,,,,tt          JJ,,iiffii                                     \n");
+    printf("                  ..LL,,,,,,,,,,,,,,,,,,,,,,,,,,tttt,,,,,,,,jj            LL;;jjLL                                     \n");
+    printf("                    ttLL,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,jj          iiGG;;jjGG                                     \n");
+    printf("                    jj,,ii,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,jj      jjLLtt;;;;jjDD                                     \n");
+    printf("                    tt;;,,,,,,,;;;;;;;;,,,,,,,,;;,,,,,,,,,iiGG;;    jjiijjffGGff                                       \n");
+    printf("                    jj,,,,,;;,,;;iijj;;,,,,,,LL,,,,,,,,,ffKKDDjj    iittjjGG                                           \n");
+    printf("                    jj,,,,,,,ff,,,,,,,,,,,,LL,,,,,,,,,,,iiDDKKGG      LLffEE                                           \n");
+    printf("                    jj,,,,,,,LL,,,,,,,,,,tttt,,,,,,,,,,,,,,,,,LL      LLkkkktt                                         \n");
+    printf("                    jj,,,,,,,ff,,,,,,,,,,LL,,,,,,,,,,,ii;;,,,,jj,,..LLKKKKKKGG                                         \n");
+    printf("                    jj,,,,,,,ff,,,,,,,,;;jj,,,,,,,,,,,GG;;,,,,,,ttLLKKKKEEjj                                           \n");
+    printf("                    ;;tt,,,,,ff,,,,,,,,ttii,,,,,,,,,LLjj,,,,,,,,jj;;KKDD                                               \n");
+    printf("                      LL,,,,,ff,,,,,,,,ff,,,,,,,,,jjtt,,,,,,,,ttGG,,EEKK                                               \n");
+    printf("                      ;LL,,;;GG,,,,,,,,LL,,,,,,,LLtt,,,,,,,,DDEEKKEEKKK                                                \n");
+    printf("                      tjjLLffii,,,,,,,,;;GGttjLL,,,,,,,,,,,,ffEEWWDD                                                   \n");
+    printf("                      jj,,,,,,,,,,,,,,,,,,,jji,,,,,,,,,,,,,,,,,,ff                                                     \n");
+    printf("                    iiii,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,ff                                                     \n");
+    printf("                    jj,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,jj                                                     \n");
+    printf("                    iiii,,,,,,,,,,;;;;;;,,,,,,,,,,,,,,,,,,,,,,tt;;                                                     \n");
+    printf("                      LL;;;;;;;;iijjjjjjii;;,,,,,,,,,,,,,,,,,,jj                                                       \n");
+    printf("                      ..GGjjjjjjjjjjjjjjjjjjii;;,,,,,,,,;;;;LL                                                         \n");
+    printf("                        jjiiffGGGGLLLLGGLLLLGGLLjjjjjjjjffLL                                                           \n");
+    printf("                      ffDDiiLL..              ..iiGGGGDDLL                                                             \n");
+    printf("                      GGDDtt                      LLDDjj                                                               \n");
+
+}
